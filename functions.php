fd5956501090b6f83c52c340520fd84d92b6044a
Didier Sampaolo <didier@lvlup.fr>
2023-08-16 17:20:17 +0200

    utilisation custom_blocks

--- functions.php
+++ functions.php
@@ -1,11 +1,23 @@
 <?php
 
-// register custom blocks
-function enqueue_custom_block_assets() {
-    register_block_type_from_metadata( __DIR__ . '/blocks/subcategories-block/build' );
-//    register_block_type_from_metadata( __DIR__ . '/blocks/breadcrumbs-block/build' );
-}
-add_action( 'init', 'enqueue_custom_block_assets' );
+require get_template_directory() . '/blocks/subcategories-block/render.php';
+require get_template_directory() . '/blocks/breadcrumbs-block/render.php';
 
-//require get_template_directory() . '/blocks/subcategories-block/render.php';
-//require get_template_directory() . '/blocks/breadcrumbs-block/render.php';
+function register_custom_blocks()
+{
+    register_block_type('lvlup-dev/subcategories-block', array(
+        'editor_script' => 'file:./blocks/subcategories-block/build/index.js', // Fichier JS de l'éditeur
+        'editor_style' => 'file:./blocks/subcategories-block/build/index.css', // Fichier CSS de l'éditeur
+        'style' => 'file:./blocks/subcategories-block/build/style.css', // Fichier CSS du front-end
+        'render_callback' => 'render_subcategories_block',
+    ));
+
+    register_block_type('lvlup-dev/breadcrumbs-block', array(
+        'editor_script' => 'file:./blocks/breadcrumbs-block/build/index.js', // Fichier JS de l'éditeur
+        'editor_style' => 'file:./blocks/breadcrumbs-block/build/index.css', // Fichier CSS de l'éditeur
+        'style' => 'file:./blocks/breadcrumbs-block/build/style.css', // Fichier CSS du front-end
+        'render_callback' => 'render_breadcrumbs_block',
+    ));
+}
+
+add_action('init', 'register_custom_blocks');
