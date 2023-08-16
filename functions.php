4e330eb805ddedc4631fcf3e2c4a2d072c2970d9
Didier Sampaolo <didier@lvlup.fr>
2023-08-16 19:04:56 +0200

    catégories: deuxième description (longue)

--- functions.php
+++ functions.php
@@ -21,3 +21,26 @@ function register_custom_blocks()
 }
 
 add_action('init', 'register_custom_blocks');
+
+
+// descriptions des catégories
+function add_category_content_field() {
+    wp_editor('', 'term_content', array('media_buttons' => true));
+}
+add_action('category_add_form_fields', 'add_category_content_field', 10, 2);
+
+// Ajouter l'éditeur WYSIWYG à la page d'édition de catégorie
+function edit_category_content_field($term) {
+    $content = get_term_meta($term->term_id, 'term_content', true);
+    wp_editor($content, 'term_content', array('media_buttons' => true));
+}
+add_action('category_edit_form_fields', 'edit_category_content_field', 10, 2);
+
+// Sauvegarder le contenu lors de l'ajout ou de la mise à jour d'une catégorie
+function save_category_content($term_id) {
+    if (isset($_POST['term_content'])) {
+        update_term_meta($term_id, 'term_content', $_POST['term_content']);
+    }
+}
+add_action('create_category', 'save_category_content');
+add_action('edit_category', 'save_category_content');
